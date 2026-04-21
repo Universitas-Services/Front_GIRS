2001355948151ba39173996c7a673ce14de374c8
Yovani Navas <yovaninavas10@gmail.com>
2026-04-21 16:27:13 -0400

    Quitar dominio de nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-    // 1. Autoriza el origen para cargar estilos, scripts y HMR (WebSockets)
-    allowedDevOrigins: ['https://consolidated-defence-making-forwarding.trycloudflare.com', '*.trycloudflare.com'],
-
-    // 2. Autoriza el origen para poder enviar formularios (Login) usando Server Actions
-    experimental: {
-        serverActions: {
-            allowedOrigins: ['https://consolidated-defence-making-forwarding.trycloudflare.com', '*.trycloudflare.com'],
-        },
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
